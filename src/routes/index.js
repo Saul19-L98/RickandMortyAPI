0352216965cefbc78622266adf5f4c5f1c36c0ac
Saúl Laínez <saul.alejandro19@gmail.com>
2020-06-30 15:56:28 -0600

    Se agregaron templates, routes y se hizo un cambio en el index de src y se modifico la configuración de webpack.

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import Header from '../Templates/Header';
+import Home from '../pages/Home';
+import Character from '../pages/Character';
+import Error404 from '../pages/Error404';
+const routes ={
+    '/': Home,
+    '/:id': Character,
+    '/contact': 'Contact',
+};
+const router = async ()=>{
+    const header = null || document.getElementById('header');
+    const content = null || document.getElementById('content');
+    header.innerHTML = await Header();
+};
+export default router;
