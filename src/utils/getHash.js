611bd29bf69f96cedd4ed41717c700117369d791
Saúl Laínez <saul.alejandro19@gmail.com>
2020-06-30 21:25:03 -0600

    Se agregaron estilos y se configuro el llamado a los datos de la Api de rick and morty

---
+++ src/utils/getHash.js
@@ -0,0 +1,4 @@
+const getHash = ()=>
+//getHash['','1','']
+ location.hash.slice(1).toLocaleLowerCase().split('/')[1] || '/';
+ export default getHash;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const copyWebpackPlugin = require('copy-webpack-plugin');
+
 
 module.exports={
     entry:'./src/index.js',
@@ -22,11 +24,17 @@ module.exports={
         ]
     },
     plugins:[
-        new HtmlWebpackPlugin({
-            inject: true,
-            template:'./public/index.html',
-            filename:'./index.html',
-        },)
-    ]
-}
+        new HtmlWebpackPlugin(
+            {
+                inject: true,
+                template:'./public/index.html',
+                filename:'./index.html',
+            }
+        ),
+        new copyWebpackPlugin({
+            patterns:[
+            {from:'./src/styles/styles.css',to:''},
+        ],}),
+    ],
+};
 
